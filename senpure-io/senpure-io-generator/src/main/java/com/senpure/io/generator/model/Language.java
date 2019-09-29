aa2a723844f9f1d3d6e45945ce0131dca8a4c7b1
senpure <senpure@senpure.com>
2019-09-29 18:09:41 +0800

    normal

--- senpure-io/senpure-io-generator/src/main/java/com/senpure/io/generator/model/Language.java
+++ senpure-io/senpure-io-generator/src/main/java/com/senpure/io/generator/model/Language.java
@@ -1,5 +1,7 @@
 package com.senpure.io.generator.model;
 
+import com.senpure.io.generator.Constant;
+
 /**
  * Language
  *
@@ -8,7 +10,7 @@ package com.senpure.io.generator.model;
  */
 public class Language {
 
-    protected  Bean bean;
+    protected Bean bean;
 
     private String namespace;
 
@@ -20,11 +22,15 @@ public class Language {
     public void setNamespace(String namespace) {
         this.namespace = namespace;
     }
+
     public Language(Bean bean) {
         this.bean = bean;
     }
 
     public String getName() {
+        if (bean.getType().equals(Constant.ENTITY_TYPE_BEAN)) {
+            return bean.getName();
+        }
         return bean.getJavaName();
     }
 
